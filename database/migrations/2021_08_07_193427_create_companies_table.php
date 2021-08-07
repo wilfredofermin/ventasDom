85a0527e6b5320cd4cadbd91ef6b9565b39b25e7
Wilfredo Fermin Reyes <wfermin@viva.com.do>
2021-08-07 16:40:04 -0400

    creacion de los modelos

---
+++ database/migrations/2021_08_07_193427_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',150);
+            $table->texte('address',255)->nullable();
+            $table->string('addressphone',10)->nullable();
+            $table->string('rnc',20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
